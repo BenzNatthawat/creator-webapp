4ae97c76a2c8d4d413712b7ac77f2b804a89e588
chada <lottochada@gmail.com>
2020-10-19 23:11:18 +0700

    19/10v.2

--- page.php
+++ page.php
@@ -16,7 +16,6 @@ get_header();
   <?php include('slider-marquee.php'); ?>
 
 	<main id="primary" class="site-main">
-
 		<?php
 		while ( have_posts() ) :
 			the_post();
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -6,51 +6,59 @@
  */
 
 ?>
-
 <div class="container">
-  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-      <?php the_title( '<h1 class="entry-title d-flex justify-content-center">', '</h1>' ); ?>
-    </header><!-- .entry-header -->
+  <div class="row">
+    <div class="col col-12 col-sm-12 col-md-8 col-lg-8">
+      <div class="box-content">
+        <div class="container">
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+              <?php the_title( '<h1 class="entry-title d-flex justify-content-center">', '</h1>' ); ?>
+            </header><!-- .entry-header -->
 
-    <div class="entry-content">
-      <?php
-      the_content();
+            <div class="entry-content">
+              <?php
+              the_content();
 
-      wp_link_pages(
-        array(
-          'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'creator-webapp' ),
-          'after'  => '</div>',
-        )
-      );
-      ?>
-    </div><!-- .entry-content -->
+              wp_link_pages(
+                array(
+                  'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'creator-webapp' ),
+                  'after'  => '</div>',
+                )
+              );
+              ?>
+            </div><!-- .entry-content -->
 
-    <?php if ( get_edit_post_link() ) : ?>
-      <footer class="entry-footer">
-        <?php
-        edit_post_link(
-          sprintf(
-            wp_kses(
-              /* translators: %s: Name of current post. Only visible to screen readers */
-              __( 'Edit <span class="screen-reader-text">%s</span>', 'creator-webapp' ),
-              array(
-                'span' => array(
-                  'class' => array(),
-                ),
-              )
-            ),
-            wp_kses_post( get_the_title() )
-          ),
-          '<span class="edit-link">',
-          '</span>'
-        );
-        ?>
-      </footer><!-- .entry-footer -->
-    <?php endif; ?>
-  </article><!-- #post-<?php the_ID(); ?> -->
+            <?php if ( get_edit_post_link() ) : ?>
+              <footer class="entry-footer">
+                <?php
+                edit_post_link(
+                  sprintf(
+                    wp_kses(
+                      /* translators: %s: Name of current post. Only visible to screen readers */
+                      __( 'Edit <span class="screen-reader-text">%s</span>', 'creator-webapp' ),
+                      array(
+                        'span' => array(
+                          'class' => array(),
+                        ),
+                      )
+                    ),
+                    wp_kses_post( get_the_title() )
+                  ),
+                  '<span class="edit-link">',
+                  '</span>'
+                );
+                ?>
+              </footer><!-- .entry-footer -->
+            <?php endif; ?>
+          </article><!-- #post-<?php the_ID(); ?> -->
+        </div>
+
+            <?php 
+            // creator_webapp_post_thumbnail();
+            ?>
+      </div>
+    </div>
+    <?php get_sidebar(); ?>
+  </div>
 </div>
-
-    <?php 
-    // creator_webapp_post_thumbnail();
-    ?>
