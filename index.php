10be53b6de4b7757f9b94b764a6fca3584153a01
Natthawat Singkala <you@example.com>
2020-11-19 15:08:06 +0700

    19/11v.1

--- index.php
+++ index.php
@@ -28,7 +28,6 @@ get_header();
           <div class="box-content">
             <?php
             if ( have_posts() ) :
-
               if ( is_home() && ! is_front_page() ) :
                 ?>
                 <header>
@@ -40,13 +39,14 @@ get_header();
               /* Start the Loop */
               while ( have_posts() ) :
                 the_post();
+                print_r(the_post());
 
                 /*
                 * Include the Post-Type-specific template for the content.
                 * If you want to override this in a child theme, then include a file
                 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
                 */
-                get_template_part( 'template-parts/content', get_post_type() );
+                get_template_part( 'template-parts/content', 'home' );
 
               endwhile;
 
